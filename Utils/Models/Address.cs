98deb92686bea22fd8ae66167d67c1081075ee01
Philip Braatz <philopbritz@gmail.com>
2024-05-15 18:30:57 -0500

    Versioning updates for 1.0.2

--- Utils/Models/Address.cs
+++ Utils/Models/Address.cs
@@ -1,8 +1,8 @@
 ﻿namespace Doorfail.Utils.Models;
 public class Address
 {
-    public string POBox { get; set; }
-    public string Street { get; set; }
+    public string Line1 { get; set; }
+    public string Line2 { get; set; }
     public string City { get; set; }
     public string State { get; set; }
     public string ZipCode { get; set; }
